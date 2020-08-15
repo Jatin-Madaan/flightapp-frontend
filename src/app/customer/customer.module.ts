6d1873ee714457f36a1a9b20a57e3fadb2ad1ab0
prithvekumar <prithve01234@gmail.com>
2020-08-15 11:35:52 +0530

    added service to customer

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { CustomerRoutingModule } from './customer-routing.module';
 import { HomeComponent } from './home/home.component';
+import { PaymentComponent } from './payment/payment.component';
+
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, PaymentComponent],
   imports: [
     CommonModule,
     CustomerRoutingModule
