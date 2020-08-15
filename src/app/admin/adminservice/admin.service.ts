686eb8f1b2eb4183e1ef4d04a181f4bdf41ca2c9
Yash Sharma <ldh.yashsharma@gmail.om>
2020-08-15 21:55:15 +0530

    2 modules mone: View & Delete

--- src/app/admin/adminservice/admin.service.ts
+++ src/app/admin/adminservice/admin.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ScheduleFlight } from 'src/app/models/scheduleFlight';
+import { Timestamp } from 'rxjs';
+import { Time } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +10,22 @@ import { HttpClient } from '@angular/common/http';
 export class AdminService {
 
   constructor(private http:HttpClient) { }
+
+  url:string="http://localhost:8085/";
+
+  getSchedulesFlights()
+  {
+    return this.http.get<ScheduleFlight[]>(this.url+"scheduleFlight/viewAll");
+  }
+
+  removeSchedule(scheduleFlightId:number)
+  {
+    console.log(scheduleFlightId);
+    return this.http.delete(this.url+"deleteSchedule/"+scheduleFlightId);
+  }
+
+  // rescheduleFlightSchedule(rescheduleId:number, arrivalTime:Time, departureTime:Time)
+  // {
+  //   return this.http.put(this.url+"/rescheduleFlightSchedule/"+rescheduleId+arrivalTime+departureTime);
+  // }
 }
--- src/app/admin/reschedule/reschedule.component.ts
+++ src/app/admin/reschedule/reschedule.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../adminservice/admin.service';
+import { ScheduleFlight } from 'src/app/models/scheduleFlight';
 
 @Component({
   selector: 'app-reschedule',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RescheduleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:AdminService) { }
 
-  ngOnInit() {
+  scheduleFlightsList:ScheduleFlight[];
+
+  ngOnInit(): void 
+  {
+    this.service.getSchedulesFlights().subscribe(data=>
+      {
+        this.scheduleFlightsList = data;
+      },
+      error=>{
+        alert("error occured while subscribing data from server");
+        console.log("error occured while subscribing data from server");
+      });
   }
 
+  deleteScheduleFlight(scheduleFlight:ScheduleFlight)
+  {
+    console.log(scheduleFlight);
+    this.service.removeSchedule(scheduleFlight.scheduleFlightId).subscribe(data => console.log(data));
+  }
+
+  reschedule(scheduleFlight:ScheduleFlight)
+  {
+    ;
+  }
+
+  cancelRescheduling()
+  {
+    ;
+  }
 }
