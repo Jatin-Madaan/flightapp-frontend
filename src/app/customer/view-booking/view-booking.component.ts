00150904440f134acaa9d69d460301688a020352
Ankit <ankit.p1497@gmail.com>
2020-08-16 12:27:00 +0530

    cancel booking added

--- src/app/customer/view-booking/view-booking.component.ts
+++ src/app/customer/view-booking/view-booking.component.ts
@@ -3,6 +3,7 @@ import { CustomerService } from '../customerservice/customer.service';
 import {Booking} from '../../models/Booking';
 import { Flight } from 'src/app/models/Flight';
 import { Passenger } from 'src/app/models/Passenger';
+declare var $: any;
 
 @Component({
   selector: 'app-view-booking',
@@ -14,7 +15,10 @@ export class ViewBookingComponent implements OnInit {
   bookings:Booking[];
   flight:Flight;
   passenger:Passenger[];
-  
+  checkFlight:boolean=false;
+  checkPassenger:boolean=false;
+  index:number;
+
   constructor(private service:CustomerService) { }
 
   ngOnInit() {
@@ -27,10 +31,25 @@ export class ViewBookingComponent implements OnInit {
   }
 
   viewFlight(index:number){
+    this.checkFlight=true;
     this.flight=this.bookings[index].flight;
   }
 
   viewPassenger(index:number){
+    this.checkPassenger=true;
     this.passenger=this.bookings[index].passengers;
+  }
+
+  setIndex(index:number){
+    this.index=index;
+  }
+
+  cancelBooking(){
+    this.service.cancelBooking(this.bookings[this.index].bookingId).subscribe(data=>{
+      this.bookings[this.index]=data;
+      $('#cancelModal').modal('hide');
+    },err=>{
+      console.log(err);
+    });
   }
 }
