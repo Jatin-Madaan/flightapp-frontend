589647310b17f64e7d11be8d9ca2c12dabd49529
Jatin <jatinmadaan9882@gmail.com>
2020-08-18 07:02:07 +0530

    error resolved

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -1,23 +1,8 @@
-<<<<<<< HEAD
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { CustomerRoutingModule } from './customer-routing.module';
-import { HomeComponent } from './home/home.component';
-import { ViewBookingComponent } from './view-booking/view-booking.component';
-import { PaymentComponent } from './payment/payment.component';
-
-import{ReactiveFormsModule, FormsModule} from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { InvoiceComponent } from './invoice/invoice.component';
-import { ModifyBookingComponent } from './modify-booking/modify-booking.component';
 import { AddBookingComponent } from './add-booking/add-booking.component';
-
-=======
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
->>>>>>> 2a835862473a46a6911ed146136caba10f872a73
-
 import { CustomerRoutingModule } from "./customer-routing.module";
 import { HomeComponent } from "./home/home.component";
 import { ViewBookingComponent } from "./view-booking/view-booking.component";
@@ -30,9 +15,6 @@ import { InvoiceComponent } from "./invoice/invoice.component";
 import { ModifyBookingComponent } from "./modify-booking/modify-booking.component";
 
 @NgModule({
-<<<<<<< HEAD
-  declarations: [HomeComponent,ViewBookingComponent, PaymentComponent, ModifyBookingComponent,InvoiceComponent, AddBookingComponent],
-=======
   declarations: [
     HomeComponent,
     ViewBookingComponent,
@@ -41,8 +23,8 @@ import { ModifyBookingComponent } from "./modify-booking/modify-booking.componen
     InvoiceComponent,
     SearchFlightComponent,
     ListFlightsComponent,
+    AddBookingComponent
   ],
->>>>>>> 2a835862473a46a6911ed146136caba10f872a73
   imports: [
     CommonModule,
     CustomerRoutingModule,
